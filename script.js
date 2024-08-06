517438d0f82db3f6608af7f5d6a2f5bc7f5538e9
Chamindu-08 <chamindu08induwara@gmail.com>
2024-08-07 02:51:55 +0530

    Implement dashboard front end

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const accountIcon = document.getElementById('accountIcon');
+    const dropdown = document.querySelector('.user .dropdown');
+
+    accountIcon.addEventListener('click', () => {
+        dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+    });
+
+    // Close the dropdown if clicked outside
+    window.addEventListener('click', (e) => {
+        if (!accountIcon.contains(e.target) && !dropdown.contains(e.target)) {
+            dropdown.style.display = 'none';
+        }
+    });
+});
